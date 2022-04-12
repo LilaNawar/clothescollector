5aa3277a89ba5a9aae6da4fe94462ef9578cead6
Lila <lila.nawar21@gmail.com>
2022-04-12 19:24:19 -0400

    Momentum lab,done

--- ajax/JS/axios.js
+++ ajax/JS/axios.js
@@ -23,6 +23,19 @@ axios({
 })
 
 
+$(document).ready(function(){    
+if (currenttime.getHours() >=12 && currenttime.getHours() <17){
+    $("body").append(`<h3>${currenttime.getHours()}:${currenttime.getMinutes()}</h3><h4>Good Afternoon, Lila</h4>`);
+} else if (currenttime.getHours() >=17 && currenttime.getHours() <20){
+    $("body").append(`<h3>${currenttime.getHours()}:${currenttime.getMinutes()}<h3><h4>Good Evening, Lila</h4>`);
+}else if ((currenttime.getHours() >= 1 && currenttime.getHours() <12 )|| currenttime.getHours()==24){
+    $("body").append(`<h3>${currenttime.getHours()}:${currenttime.getMinutes()}<h3><h4>Good Morning Lila!</h4>`);
+}
+else if (currenttime.getHours() >= 20 && currenttime.getHours() <24){
+    $("body").append(`<h3>${currenttime.getHours()}:${currenttime.getMinutes()}</h3><h4>Good Night, Lila</h4>`);
+}})
+
+
 axios({
     method: 'get',
     url: "https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json"
@@ -32,19 +45,32 @@ axios({
     $("body").append(`<h3>${currenttime.getHours()}:${currenttime.getMinutes()}</h3>`)
     $('body').append(`<h1 class="bottom-center">${response.data.quoteText} - ${response.data.quoteAuthor}</h1>`)
     // $("h1").css({"position": "absolute", "bottom": "0", "width": "100%","height": "2.5rem"})
+
 })
 
 .catch((error) => {
     console.log("your mom")
 })
+
+
 axios({
     method: 'get',
     url: "https://api.openweathermap.org/data/2.5/weather?q=Toronto&appid=90207e2df89e95452154840f30f8163b&units=metric"
   })
 .then((response) =>{
-    console.log(Math.round(response.data.main.temp,1))
+    const icons = {
+        clear: '☀',
+        rain: '️🌧',
+        storm: '⛈',
+        snow: '🌨',
+        mist: '🌫',
+        clouds: '☁',
+      };
+    
     roundedTemp = Math.round(response.data.main.temp,1)
-    $('body').append(`<h2 class="temp">${roundedTemp} °C </h2>`)
+    cityname= response.data.name
+    console.log(response.data.weather[0].main.toLowerCase())
+    $('body').append(`<h2 class="temp">${icons[response.data.weather[0].main.toLowerCase()]} ${roundedTemp}°C </h2>`)
 })
 .catch((error) => {
     console.log("your mom")
