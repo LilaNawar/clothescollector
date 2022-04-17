1be221c53878305fbea26a609db144680a4b3f0e
Lila <76576058+LilaNawar@users.noreply.github.com>
2022-04-17 17:06:37 -0400

    Update models.py

--- mycollector/mycollector/main_app/models.py
+++ mycollector/mycollector/main_app/models.py
@@ -2,53 +2,48 @@ from asyncio.windows_events import NULL
 from secrets import choice
 from django.db import models
 from django.urls import reverse
-from datetime import date
-
 # Create your models here.
-
-
-MEALS = (
-    ('B', 'Breakfast'),
-    ('L', 'Lunch'),
-    ('D', 'Dinner')
+OUTFIT = (
+    ('W', 'Work'),
+    ('C', 'Casual'),
+    ('N', 'Night out')
 )
 
-class Toy(models.Model):
+class Pants(models.Model):
     name = models.CharField(max_length=50)
     color = models.CharField(max_length=20)
+    size = models.CharField(max_length=100)
 
     def __str__(self):
         return self.name
 
     def get_absolute_url(self):
-        return reverse('toys_detail', kwargs={'pk': self.id})
-
-
-class Cat(models.Model):
-    name = models.CharField(max_length=100)
-    breed = models.CharField(max_length=100)
-    description = models.TextField(max_length=250)
-    age = models.IntegerField()
-    image = models.CharField(default=None, blank=True, null=True, max_length=2000)
-    toys = models.ManyToManyField(Toy)
-
-    def get_absolute_url(self):
-        return reverse('detail', kwargs = {'cat_id': self.id})
-
-    def __str__(self):
-        return self.name
-
-    def fed_for_today(self):
-        return self.feeding_set.filter(date=date.today()).count() >= len(MEALS)
-
-
-class Feeding(models.Model):
-    date = models.DateField('Feeding Date')
-    meal = models.CharField(max_length=1, choices=MEALS, default=MEALS[0][0])
-    cat = models.ForeignKey(Cat, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return f"{self.get_meal_display()} on {self.date}"
-
+        return reverse('pant_detail', kwargs={'pk': self.id})
+        
     class Meta:
-        ordering = ['-date']
+      ordering = [-1]
+
+
+class Top(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(max_length=250)
+    image = models.CharField(default=None, blank=True, null=True, max_length=2000)
+    # pants = models.ManyToManyField(Pant)
+    size = models.CharField(max_length=100)
+
+    def get_absolute_url(self):
+        return reverse('detail', kwargs = {'top_id': self.id})
+
+    def __str__(self):
+        return self.name
+    class Meta:
+        ordering = ['name']
+
+
+
+class Occasion(models.Model):
+    top = models.ForeignKey(Top, on_delete=models.CASCADE)
+    pant = models.ForeignKey(Pants, on_delete=models.CASCADE)
+
+
+
